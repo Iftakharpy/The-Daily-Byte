ecf26852138daa4485aff913585db32fe3a1813d
Iftakhar Ahmed <iftakharn19@gmail.com>
2020-10-11 19:49:40 +0600

    added two pass solution

--- 012. Remove Nth to Last Node/remove_nth_to_last_node.py
+++ 012. Remove Nth to Last Node/remove_nth_to_last_node.py
@@ -10,7 +10,11 @@ class Node:
 		return repr(self) == repr(other)
 
 
+# this is the one pass technique
+# O(n) time traversing the hole linked list
+# O(1) space only storing references
 def remove_nth_to_last_node(head: Node, n: int) -> Node:
+	# this dummy node is to help solve the corner cases
 	dumb = Node(0,head)
 	end_checker = dumb
 	back = dumb
@@ -28,5 +32,31 @@ def remove_nth_to_last_node(head: Node, n: int) -> Node:
 	back.next = back.next.next
 	#returning the reference
 	return dumb.next
+# remove_nth_to_last_node(Node(1, Node(2, Node(4))), 3)
+# remove_nth_to_last_node(Node(1, Node(2, Node(4, Node(5)))), 3)
+
+
+# this is the two pass technique
+# O(2n) -> O(n) time traversing the linked list twice
+# O(1) space storing reference
+def remove_nth_to_last_node(head: Node, n: int) -> Node:
+	ref = head
+	length = 0
+	# getting the length
+	while ref!=None:
+		length+=1
+		ref = ref.next
+	
+	# dummy node helps with the corner cases like removing the first node
+	dumb = Node(0,head)
+	ref = dumb
+	# traversing to the node before the target node
+	for i in range(length-n):
+		ref = ref.next
+	# removing the target node
+	ref.next = ref.next.next
+	
+	return dumb.next
+
 # remove_nth_to_last_node(Node(1, Node(2, Node(4))), 3)
 # remove_nth_to_last_node(Node(1, Node(2, Node(4, Node(5)))), 3)
